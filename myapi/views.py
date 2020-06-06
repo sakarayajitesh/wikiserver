f7eb39a6202c94708c4f65669d96edba29589040
Ajitesh Sakaray <sakarayajitesh@hotmail.com>
2020-06-06 13:16:35 +0530

    feat: add passing multiple text in header

--- myapi/views.py
+++ myapi/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import wikipedia
+import json
 
 
 # Create your views here.
 class Summary(APIView):
 
     def get(self, request):
-        text = request.headers['text']
-        return Response(wikipedia.summary(text))
+        try:
+            textList = request.headers['textlist']
+            responseList = []
+            for text in json.loads(textList):
+                print("requesting = ", text)
+                try:
+                    response = wikipedia.summary(text, sentences=3)
+                    responseList.append({"text": text, "result": response})
+                except Exception as e:
+                    print(e)
+            return Response(responseList)
+        except Exception as e:
+            print(e)
